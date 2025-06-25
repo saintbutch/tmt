b08dad7ab0f1f82636ab438792d1b697a06e9cfe
saintbutch <xyxephrien@gmail.com>
2025-06-25 20:42:51 +1200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,8 +14,7 @@ addLayer("layer_mana", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(0),
-		if (hasUpgrade('layer_mana', 11)) gain = gain.add(1),
+        mult = new Decimal(0)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
